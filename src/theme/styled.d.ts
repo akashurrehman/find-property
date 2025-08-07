f250a271b61e90e279a1e50282e45825a39a5f65
Akash Ur Rehman <akashurrehman456@gmail.com>
2025-08-08 00:21:39 +0300

    fix: add dummy property to DefaultTheme to prevent empty interface error

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -1,8 +1,9 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
 import 'styled-components';
 import { ThemeType } from './theme';
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
+  // Add a dummy property to avoid empty interface error
+  export interface DefaultTheme extends ThemeType {
+    __nonEmpty?: true;
+  }
 }
-/* eslint-enable @typescript-eslint/no-empty-interface */
